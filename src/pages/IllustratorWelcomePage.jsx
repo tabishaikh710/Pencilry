d465a09fa0c01dad0ca3490e20bb93ddc8e7a658
Muhammad Tabish Shaikh <144883941+tabishaikh710@users.noreply.github.com>
2024-11-30 02:29:14 +0500

    illustretor new update

--- src/pages/IllustratorWelcomePage.jsx
+++ src/pages/IllustratorWelcomePage.jsx
@@ -6,6 +6,20 @@ function IllustratorWelcomePage() {
 
             <div className="welcomePageContent">
             <h1>Welcome  Illustrator Ready for your next big opportunity?</h1>
+            <ul>
+                <li>Answer a few questions and start building your profile</li>
+                <li>Apply for open roles or list services for clients to buy</li>
+                <li>Get paid safely and know we’re there to help</li>
+            </ul>
+            <div className={style.bottomIllustretoWelcome}> 
+            <button className={style.Ilbutton}>
+                Get started
+            </button>
+            <p>
+            It only takes 5-10 minutes <br/> you can edit it later. We’ll save as you go.
+            </p>
+            </div>
+            
             </div>
             <div className="welcomePageContent">
             <h1>Welcome  Illustrator Ready for your next big opportunity?</h1>
