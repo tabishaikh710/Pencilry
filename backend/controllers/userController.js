f2676353e507b14f7ef49609cbfbe37432eee7e3
Muhammad Tabish Shaikh <144883941+tabishaikh710@users.noreply.github.com>
2025-02-04 02:48:26 +0500

    email verification

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 const User = require("../models/userModel")
 const bcrypt= require('bcrypt'); 
-
+const mailer = require('../helpers/mailer');
 const {validationResult}=require('express-validator')
 
 const userRegister = async (req, res) => {
@@ -9,7 +9,19 @@ const userRegister = async (req, res) => {
 
 try {
 
-    validationResult(req);
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+
+        return res.status(400).json({
+            success:false,
+            msg:'error',
+            errors: errors.array()
+            
+            });
+
+        
+    }
 
 const {name , email , mobile, password} = req.body;
 
@@ -31,6 +43,12 @@ const user = new User({
 });
 
 const userData= await user.save();
+
+const msg=`<p>hii ${name}, please <a href="http://localhost:4000/mail-varification?id=${userData._id}">click here to varify</a> your email</p>`;
+
+mailer.sendMail(email,'mail varification', msg);
+
+
 
 return res.status(200).json({
     success:true,
---
+++ backend/helpers/mailer.js
@@ -0,0 +1,51 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+   secure:false,
+   requireTLS:true,
+   auth:{
+    user: process.env.SMTP_MAIL,
+    pass:process.env.SMTP_PASSWORD
+   }
+
+
+    
+});
+
+
+
+const sendMail = async (email, subject, content) => {
+    try {
+        var mailOptions = {
+            from: process.env.SMTP_MAIL,
+            to:email,
+            subject: subject,
+            html:content
+        }
+         
+        transporter.sendMail(mailOptions,(error,info)=>{
+            if(error){
+                console.log(error);
+                
+            }
+           console.log('mail sent', info.messageId);
+
+
+
+
+        });
+
+
+
+    } 
+    catch (error) {
+        console.log(error.message)
+    }
+}
+
+
+module.exports={
+    sendMail
+}
