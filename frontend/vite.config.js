08e279d949afb35f5ab27ebd921eacbedccb63b7
Muhammad Tabish Shaikh <144883941+tabishaikh710@users.noreply.github.com>
2025-04-08 21:07:03 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './', // Add this line
+  build: {
+    outDir: 'dist', // this is the default, you can omit it if not customized
+  },
 })
